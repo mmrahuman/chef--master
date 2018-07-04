2cf1fdba75f659e178cc91219f9ac7df0d809c65
Patrick Roughan <vagrant@localhost.localdomain>
2018-07-04 09:27:00 +0000

    setup.rb

---
+++ cookbooks/workstation/recipes/setup.rb
@@ -0,0 +1,18 @@
+package 'tree' do
+	action :install
+end
+
+package 'ntp' do
+	action :install
+end
+
+file '/etc/motd' do
+	content 'This server is....'
+	action :create
+	owner 'root'
+	group 'root'
+end
+
+service 'ntpd' do
+	action [ :enable, :start ]
+end
