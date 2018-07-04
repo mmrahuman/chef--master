f07a16c0970b039ab99f3fc78328a30b921077b6
Patrick Roughan <vagrant@localhost.localdomain>
2018-07-04 13:51:50 +0000

    adding image

--- cookbooks/apache-server/recipes/server.rb
+++ cookbooks/apache-server/recipes/server.rb
@@ -2,6 +2,14 @@ package 'httpd' do
 	action :install
 end
 
+cookbook_file 'var/www/html/example.html' do
+	source 'example.html'
+end
+
+remote_file '/var/www/html/pic.png' do
+	source 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/PNG_transparency_demonstration_1.png/280px-PNG_transparency_demonstration_1.png'
+end
+
 template '/var/www/html/index.html' do
 	source 'index.html.erb'
 	variables(
