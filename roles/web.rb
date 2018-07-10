479bc0ba848c026c7305a84726f4ae7e9a063fcb
patrick <patrick@roguetech-command-control.lan>
2018-07-10 06:39:47 +0100

    adding new users

--- roles/web.rb
+++ roles/web.rb
@@ -1,6 +1,6 @@
 name 'web'
 description 'web server role'
-run_list "recipe[workstation]","recipe[apache-server]"
+run_list "role[base]","recipe[workstation]","recipe[apache-server]"
 default_attributes 'apache-test' => {
 	'attribute1' => 'hello from attribute 1',
 	'attribute2' => 'whats up'
